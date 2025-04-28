a58ab445f6c23475a404853f682d85f0909f6067
JesusGar13 <82331712+jesus13g@users.noreply.github.com>
2025-04-29 00:08:59 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 # run.py
 from app import create_app
+from app.models.db_connection import Database
 
 app = create_app()
+
+# Crear las tablas en el contexto de la app
+with app.app_context():
+    db_instance = Database()
+    db = db_instance.get_db()
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
